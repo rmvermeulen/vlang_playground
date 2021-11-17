26f5a51987184361b8080d690140f7c0c8d03248
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-11-17 22:40:09 +0100

    doesnt work

---
+++ di/container.v
@@ -0,0 +1,34 @@
+module di
+
+pub interface Builder {
+	register()
+}
+
+pub interface Context {
+	resolve()
+}
+
+[heap]
+pub struct Service {}
+
+pub struct Container {
+mut:
+	services  map[string]Service
+	factories map[string]Factory<di.Service>
+}
+
+pub type Factory = fn (ctx Context) &Service
+
+pub fn (c Container) register(token string, factory Factory) {
+	c.factories[token] = factory
+}
+
+pub fn (c Container) resolve(token string) ?&Service {
+	if token in c.services {
+		return &c.services[token]
+	}
+	if token in c.factories {
+		return &c.factories[token]
+	}
+	return none
+}
--- vlang_playground.v
+++ vlang_playground.v
@@ -1,5 +1,15 @@
 module main
 
+import di { Container, Context }
+
+[heap]
+struct ServiceA {}
+
+[heap]
+struct ServiceB {
+	service_a &ServiceA
+}
+
 fn main() {
 	println('Hello World!')
 	nums := []int{len: 10, init: it}
@@ -8,4 +18,14 @@ fn main() {
 	println(dbls)
 	sqrs := nums.map(it * it)
 	println(sqrs)
+
+	// container section
+	c := Container{}
+	c.register<ServiceA>('service-a', fn (ctx Context) ServiceA {
+		return ServiceA{}
+	})
+	c.register<ServiceB>('service-b', fn (ctx Context) ServiceB {
+		service_a := ctx.resolve<ServiceA>('service-a') or { panic('Failed to find service') }
+		return ServiceB{service_a}
+	})
 }
