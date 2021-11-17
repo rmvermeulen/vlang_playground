17826663f8d3e4772415ec1aaf82cdb53993647f
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-11-17 22:08:10 +0100

    some V mapping stuff

--- vlang_playground.v
+++ vlang_playground.v
@@ -2,4 +2,10 @@ module main
 
 fn main() {
 	println('Hello World!')
+	nums := []int{len: 10, init: it}
+	println(nums)
+	dbls := nums.map(it * 2)
+	println(dbls)
+	sqrs := nums.map(it * it)
+	println(sqrs)
 }
